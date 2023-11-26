83db1cf1e92866d47c398100c86d54f326412b10
EmreAras04 <emre.araspro@gmail.com>
2023-11-26 20:33:52 +0100

    Initial Commit 1

--- src/main/java/com/exemple/App.java
+++ src/main/java/com/exemple/App.java
@@ -4,14 +4,9 @@ package com.exemple;
  * Hello world!
  *
  */
-public class App { 
-    public String getGreet() {
-        return "Hello, World!";
-    }
-
+public class App {
     public static void main(String[] args) {
-        App app = new App();
-        System.out.println(app.getGreeting());
+        System.out.println("Hello,World!");
     }
 }
 
